d5a9c597724e965393e82784ec350c01ac605093
Ahmad Nurdianto <ahmadhky@gmail.com>
2023-09-14 01:58:43 +0700

    kombinasi warna nav

--- resources/views/Pages/master.blade.php
+++ resources/views/Pages/master.blade.php
@@ -53,7 +53,7 @@
         </div> --}}
 
         <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <nav class="main-header navbar navbar-expand navbar-teal navbar-light">
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -199,19 +199,20 @@
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <aside class="main-sidebar sidebar-light-primary elevation-6">
             <!-- Brand Logo -->
-            <a href="" class="brand-link">
-                <img src="dist/img/AdminLTELogo.png" alt="" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
-                <span class="brand-text font-weight-light">Klinik Asla</span>
+            <a href="" class="brand-link bg-purple">
+                <img src="" alt="" class="brand-image img-circle elevation-3" style="opacity: .8">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="brand-text font-weight-light"><i
+                        class="fa fa-hospital"></i>&nbsp; Klinik
+                    Asla</span>
             </a>
             <!-- Sidebar -->
             <div class="sidebar">
                 <!-- Sidebar user panel (optional) -->
                 <!-- SidebarSearch Form -->
                 <div class="form-inline">
-                    <div class="input-group" data-widget="sidebar-search">
+                    {{-- <div class="input-group" data-widget="sidebar-search">
                         <input class="form-control form-control-sidebar" type="search" placeholder="Search"
                             aria-label="Search">
                         <div class="input-group-append">
@@ -219,7 +220,7 @@
                                 <i class="fas fa-search fa-fw"></i>
                             </button>
                         </div>
-                    </div>
+                    </div> --}}
                 </div>
 
                 <!-- Sidebar Menu -->
