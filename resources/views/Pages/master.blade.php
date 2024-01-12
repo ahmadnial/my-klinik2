a30a22563af518b7c2dfdee573c8f6346eeee2d9
ahmadnial <ahmadhky@gmail.com>
2024-01-12 14:12:09 +0700

    tes

--- resources/views/Pages/master.blade.php
+++ resources/views/Pages/master.blade.php
@@ -480,7 +480,7 @@
                                     <li class="nav-item">
                                         <a href="{{ url('/pricelist') }}" class="nav-link">
                                             <i class="far fa-circle nav-icon"></i>
-                                            <p>Pricelist</p>
+                                            <p>Pricelist Barang</p>
                                         </a>
                                     </li>
                                     {{-- <li class="nav-item">
