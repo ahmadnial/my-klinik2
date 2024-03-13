2edaa730aba98a00870db61a5b1b06a718a99d95
ahmadnial <ahmadhky@gmail.com>
2024-03-13 23:00:41 +0700

    perbaikan redirect

--- app/Http/Controllers/kasirPoliController.php
+++ app/Http/Controllers/kasirPoliController.php
@@ -131,25 +131,8 @@ class kasirPoliController extends Controller
 
         ]);
         DB::beginTransaction();
-        // try {
-        if ($request->trs_kp_nm_tarif == null) {
-            $newrgout = new ta_registrasi_keluar();
-            $newrgout->kd_trs_reg_out = $request->kd_trs_reg_out;
-            $newrgout->trs_kp_kd_reg = $request->trs_kp_kd_reg;
-            $newrgout->trs_kp_tgl_keluar  = $request->trs_kp_tgl_keluar;
-            $newrgout->trs_kp_nm_pasien    = $request->trs_kp_nm_pasien;
-            $newrgout->trs_kp_no_mr = $request->trs_kp_no_mr;
-            $newrgout->trs_kp_layanan = $request->trs_kp_layanan;
-            $newrgout->trs_kp_dokter = $request->trs_kp_dokter;
-            $newrgout->user   = Auth::user()->name;
-            $newrgout->nm_tarif_dasar = $request->nm_tarif_dasar;
-            $newrgout->trs_kp_nilai_total = $request->trs_kp_nilai_total;
-            // $newrgout->chart_A    = $request->chart_A;
-            // $newrgout->chart_A_diagnosa = $request->chart_A_diagnosa;
-            // $newrgout->chart_P = $request->chart_P;
-            $newrgout->save();
-        } else {
-            foreach ($request->trs_kp_nm_tarif as $key => $val) {
+        try {
+            if ($request->trs_kp_nm_tarif == null) {
                 $newrgout = new ta_registrasi_keluar();
                 $newrgout->kd_trs_reg_out = $request->kd_trs_reg_out;
                 $newrgout->trs_kp_kd_reg = $request->trs_kp_kd_reg;
@@ -160,56 +143,73 @@ class kasirPoliController extends Controller
                 $newrgout->trs_kp_dokter = $request->trs_kp_dokter;
                 $newrgout->user   = Auth::user()->name;
                 $newrgout->nm_tarif_dasar = $request->nm_tarif_dasar;
-                $newrgout->trs_kp_kd_trs_chart = $request->trs_kp_kd_trs_chart[$key];
-                $newrgout->trs_kp_nm_tarif = $request->trs_kp_nm_tarif[$key];
-                $newrgout->trs_kp_nilai_tarif = $request->trs_kp_nilai_tarif[$key];
                 $newrgout->trs_kp_nilai_total = $request->trs_kp_nilai_total;
+                // $newrgout->chart_A    = $request->chart_A;
+                // $newrgout->chart_A_diagnosa = $request->chart_A_diagnosa;
+                // $newrgout->chart_P = $request->chart_P;
                 $newrgout->save();
+            } else {
+                foreach ($request->trs_kp_nm_tarif as $key => $val) {
+                    $newrgout = new ta_registrasi_keluar();
+                    $newrgout->kd_trs_reg_out = $request->kd_trs_reg_out;
+                    $newrgout->trs_kp_kd_reg = $request->trs_kp_kd_reg;
+                    $newrgout->trs_kp_tgl_keluar  = $request->trs_kp_tgl_keluar;
+                    $newrgout->trs_kp_nm_pasien    = $request->trs_kp_nm_pasien;
+                    $newrgout->trs_kp_no_mr = $request->trs_kp_no_mr;
+                    $newrgout->trs_kp_layanan = $request->trs_kp_layanan;
+                    $newrgout->trs_kp_dokter = $request->trs_kp_dokter;
+                    $newrgout->user   = Auth::user()->name;
+                    $newrgout->nm_tarif_dasar = $request->nm_tarif_dasar;
+                    $newrgout->trs_kp_kd_trs_chart = $request->trs_kp_kd_trs_chart[$key];
+                    $newrgout->trs_kp_nm_tarif = $request->trs_kp_nm_tarif[$key];
+                    $newrgout->trs_kp_nilai_tarif = $request->trs_kp_nilai_tarif[$key];
+                    $newrgout->trs_kp_nilai_total = $request->trs_kp_nilai_total;
+                    $newrgout->save();
+                }
             }
+
+            $newrekening1 = new rekening_pendapatan_poliklinik_total();
+            $newrekening1->rk_kd_reg = $request->trs_kp_kd_reg;
+            $newrekening1->rk_tgl_regout = $request->trs_kp_tgl_keluar;
+            $newrekening1->rk_no_mr  = $request->trs_kp_no_mr;
+            $newrekening1->rk_pasienName  = $request->trs_kp_nm_pasien;
+            $newrekening1->rk_dokter  = $request->trs_kp_dokter;
+            $newrekening1->rk_layanan    = $request->trs_kp_layanan;
+            $newrekening1->rk_nilai    = $request->trs_kp_nilai_total;
+            $newrekening1->rk_session_poli    = $request->session_poli;
+            $newrekening1->save();
+
+            DB::table('ta_registrasi')->where('fr_kd_reg', $request->trs_kp_kd_reg)->update([
+                'fr_tgl_keluar' => $request->trs_kp_tgl_keluar,
+            ]);
+
+            DB::table('tc_mr')->where('fs_mr', $request->trs_kp_no_mr)->update([
+                'fs_last_tarif_dasar' => $request->nm_tarif_dasar,
+                'fs_tgl_kunjungan_terakhir' => $request->trs_kp_tgl_keluar,
+            ]);
+
+            DB::commit();
+
+            $sessionFlash = [
+                'message' => 'Saved!',
+                'alert-type' => 'success'
+            ];
+
+            return Redirect::to('/kasir-poli')->with($sessionFlash);
+            // toastr()->success('Saved!');
+            // return back();
+
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            $sessionFlashErr = [
+                'message' => 'Error!',
+                'alert-type' => 'error'
+            ];
+            return Redirect::to('/kasir-poli')->with($sessionFlashErr);
+            // toastr()->error('Some Error Occured!');
+            // return back();
         }
-
-        $newrekening1 = new rekening_pendapatan_poliklinik_total();
-        $newrekening1->rk_kd_reg = $request->trs_kp_kd_reg;
-        $newrekening1->rk_tgl_regout = $request->trs_kp_tgl_keluar;
-        $newrekening1->rk_no_mr  = $request->trs_kp_no_mr;
-        $newrekening1->rk_pasienName  = $request->trs_kp_nm_pasien;
-        $newrekening1->rk_dokter  = $request->trs_kp_dokter;
-        $newrekening1->rk_layanan    = $request->trs_kp_layanan;
-        $newrekening1->rk_nilai    = $request->trs_kp_nilai_total;
-        $newrekening1->rk_session_poli    = $request->session_poli;
-        $newrekening1->save();
-
-        DB::table('ta_registrasi')->where('fr_kd_reg', $request->trs_kp_kd_reg)->update([
-            'fr_tgl_keluar' => $request->trs_kp_tgl_keluar,
-        ]);
-
-        DB::table('tc_mr')->where('fs_mr', $request->trs_kp_no_mr)->update([
-            'fs_last_tarif_dasar' => $request->nm_tarif_dasar,
-            'fs_tgl_kunjungan_terakhir' => $request->trs_kp_tgl_keluar,
-        ]);
-
-        DB::commit();
-
-        $sessionFlash = [
-            'message' => 'Saved!',
-            'alert-type' => 'success'
-        ];
-
-        return Redirect::to('/kasir-poli')->with($sessionFlash);
-        // toastr()->success('Saved!');
-        // return back();
-
-        // } catch (\Exception $e) {
-        DB::rollback();
-
-        $sessionFlashErr = [
-            'message' => 'Error!',
-            'alert-type' => 'error'
-        ];
-        return Redirect::to('/kasir-poli')->with($sessionFlashErr);
-        // toastr()->error('Some Error Occured!');
-        // return back();
-        // }
     }
 
     public function getDetailRegOut(Request $request)
