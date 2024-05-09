f79b91ce51c48eff6f9eed05f2d5b46e86cea9ee
ahmadnial <ahmadhky@gmail.com>
2024-05-09 23:24:38 +0700

    new informasi obat defacta

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,8 +18,15 @@ class DashboardController extends Controller
         // $isListPenjualan = tp_hdr::whereyear('tgl_trs', '=', $yearNow)->whereMonth('tgl_trs', '=', $monthNow)->orderBy('created_at', 'desc')->get();
         $isFakturTempo = HutangSupplier::whereyear('hs_tanggal_tempo', '=', $yearNow)->whereMonth('hs_tanggal_tempo', '=', $monthNow)->where('isLunas', '=', '0')->orderBy('hs_tanggal_tempo', 'desc')->get();
 
+        $isDefacta = DB::table('tb_stock')
+            ->where('qty', '<=', 20)
+            ->leftJoin('mstr_obat', 'tb_stock.kd_obat', 'mstr_obat.fm_kd_obat')
+            // ->leftJoin('tp_detail_item', 'tb_stock.kd_obat', 'mstr_obat.fm_kd_obat')
+            ->get();
+
         return view('Pages.index', [
-            'isFakturTempo' => $isFakturTempo
+            'isFakturTempo' => $isFakturTempo,
+            'isDefacta' => $isDefacta
         ]);
     }
 }
