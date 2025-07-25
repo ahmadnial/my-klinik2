f96d35ed65ed6238eb4cd32c6b7f2bbdf3a64247
ahmadnial <ahmadhky@gmail.com>
2025-07-25 21:44:06 +0700

    add user ID untuk memunculkan tarif dasar manual

--- resources/views/Pages/tindakan-medis.blade.php
+++ resources/views/Pages/tindakan-medis.blade.php
@@ -1183,7 +1183,7 @@
                 </div>
                 <div class="modal-body">
                     <div class="container">
-                        @if (Auth::user()->id == 4 || Auth::user()->id == 13 || Auth::user()->id == 14)
+                        @if (Auth::user()->id == 4 || Auth::user()->id == 18 || Auth::user()->id == 14)
                             <div class="">
                                 <div class="callout callout-danger bg-light">
                                     <label for="">Tarif Dasar</label>
